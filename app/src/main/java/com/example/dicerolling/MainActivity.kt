d75327edc298c960cf3c7f6c7736e047b6c47e80
Stanislaw Abyszkin <>
2019-03-16 21:16:39 +0000

    added drawable resources of dices

--- app/src/main/java/com/example/dicerolling/MainActivity.kt
+++ app/src/main/java/com/example/dicerolling/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.dicerolling
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.text_view)
+
+        val randomInt = Random().nextInt(6) + 1
+        Toast.makeText(this, "rand value: " + randomInt.toString(), Toast.LENGTH_SHORT).show()
+        resultText.text = randomInt.toString()
     }
 }

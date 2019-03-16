3c7ea7c519106ce9255090b17ed92b4033adaa4c
Stanislaw Abyszkin <>
2019-03-16 22:36:40 +0000

    finished dice rolling app

--- app/src/main/java/com/example/dicerolling/MainActivity.kt
+++ app/src/main/java/com/example/dicerolling/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.dicerolling
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +19,23 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        this.diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.text_view)
-
         val randomInt = Random().nextInt(6) + 1
-        Toast.makeText(this, "rand value: " + randomInt.toString(), Toast.LENGTH_SHORT).show()
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_6
+        }
+
+        this.diceImage.setImageResource(drawableResource)
     }
 }
